15494c1db2d09521d25fce0c1cc72fffc6356fa0
Sandip Kumar Dey <96774612+sandipkumardey@users.noreply.github.com>
2024-02-18 19:15:50 +0530

    Add files via upload

---
+++ Leetcode/DSA/MedianSortedArrays.java
@@ -0,0 +1,31 @@
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+        int[] merged = new int[m + n];
+        int i = 0, j = 0, k = 0;
+
+        while (i < m && j < n) {
+            if (nums1[i] <= nums2[j]) {
+                merged[k++] = nums1[i++];
+            } else {
+                merged[k++] = nums2[j++];
+            }
+        }
+
+        while (i < m) {
+            merged[k++] = nums1[i++];
+        }
+
+        while (j < n) {
+            merged[k++] = nums2[j++];
+        }
+
+        int totalLength = m + n;
+        if (totalLength % 2 == 0) {
+            return (merged[totalLength / 2 - 1] + merged[totalLength / 2]) / 2.0;
+        } else {
+            return merged[totalLength / 2];
+        }
+    }
+}
---
+++ Leetcode/DSA/StringToInteger.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int myAtoi(String s) {
+        int index = 0, sign = 1, total = 0;
+
+        if (s.length() == 0)
+            return 0;
+
+        while (index < s.length() && s.charAt(index) == ' ')
+            index++;
+
+        if (index < s.length() && (s.charAt(index) == '+' || s.charAt(index) == '-')) {
+            sign = s.charAt(index) == '-' ? -1 : 1;
+            index++;
+        }
+
+        while (index < s.length()) {
+            int digit = s.charAt(index) - '0';
+            
+            if (digit < 0 || digit > 9)
+                break;
+
+            if (Integer.MAX_VALUE / 10 < total || (Integer.MAX_VALUE / 10 == total && Integer.MAX_VALUE % 10 < digit))
+                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+
+            total = total * 10 + digit;
+            index++;
+        }
+
+        return total * sign;
+    }
+}
