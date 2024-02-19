3de70d83fe762160e0fff3d5b68f7adde8e74d14
Sandip Kumar Dey <96774612+sandipkumardey@users.noreply.github.com>
2024-02-19 12:16:18 +0530

    Add files via upload

---
+++ Leetcode/DSA/Container_With_Most_Water.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int max_area, area;
+        for(int i = 0; i < height.length; i++){
+            for(int j = i+1; i < height.length; j++){
+                area = hei
+            }
+        }
+    }
+}
