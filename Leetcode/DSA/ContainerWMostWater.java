bc7679b1a13c61e27e90b330118fc7f78537986f
Sandip Kumar Dey <96774612+sandipkumardey@users.noreply.github.com>
2024-02-19 14:59:56 +0530

    Add files via upload

---
+++ Leetcode/DSA/ContainerWMostWater.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int maxArea = 0;
+        int left = 0;
+        int right = height.length - 1;
+        
+        while (left < right) {
+            int minHeight = Math.min(height[left], height[right]);
+            int currentArea = minHeight * (right - left);
+            maxArea = Math.max(maxArea, currentArea);
+            
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        
+        return maxArea;
+    }
+}
