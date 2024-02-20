7e4bcc14e447d70f838355a81e44432b987b0876
Sandip Kumar Dey <96774612+sandipkumardey@users.noreply.github.com>
2024-02-20 10:12:25 +0530

    Add files via upload

---
+++ Leetcode/DSA/MergeTwoSortedLists.java
@@ -0,0 +1,25 @@
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode();
+        ListNode current = dummy;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                current.next = l1;
+                l1 = l1.next;
+            } else {
+                current.next = l2;
+                l2 = l2.next;
+            }
+            current = current.next;
+        }
+
+        if (l1 != null) {
+            current.next = l1;
+        } else {
+            current.next = l2;
+        }
+
+        return dummy.next;
+    }
+}
---
+++ Leetcode/DSA/SwapNodesInPairs.java
@@ -0,0 +1,18 @@
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode current = dummy;
+
+        while (current.next != null && current.next.next != null) {
+            ListNode first = current.next;
+            ListNode second = current.next.next;
+            first.next = second.next;
+            second.next = first;
+            current.next = second;
+            current = current.next.next;
+        }
+
+        return dummy.next;
+    }
+}
